63085c64310c9ead85033d670646687d02a35df8
Prajwol Chhetri <prajwalc391@gmail.com>
2021-08-02 20:45:02 +0545

    Added user login with Email and JWT authentication

--- authentication/tests/test_users_api.py
+++ authentication/tests/test_users_api.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django.urls import reverse
 
 from rest_framework.test import APIClient
@@ -7,7 +7,8 @@ from rest_framework import status
 
 
 REGISTER_USER_URL = reverse('auth:register')
-
+LOGIN_URL = reverse('auth:token_obtain_pair')
+REFRESH_URL = reverse('auth:token_refresh')
 
 def register_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -58,3 +59,64 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = register_user(
+            email='test@testing.com',
+            password='testpass',
+            first_name='Test',
+            last_name='User'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    
+    def test_login_success(self):
+        """Testing log-in is successful and jwt tokens are generated for registered user"""
+        payload = {'email': 'test@testing.com', 'password': 'testpass'}
+        res = self.client.post(LOGIN_URL, payload)
+
+        self.assertIn('refresh', res.data)
+        self.assertIn('access', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_login_fail_invalid_credentials(self):
+        """Testing log-in fails and jwt tokens are not generated if user provides wrong password"""
+        payload = {'email': 'test@testing.com', 'password': '2222222'}
+        res = self.client.post(LOGIN_URL, payload)
+
+        self.assertNotIn('refresh', res.data)
+        self.assertNotIn('access', res.data)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_login_fail_missing_field(self):
+        """Test that email and password are required"""
+        payload = {'email': 'test@testing.com', 'password': ''}
+        res = self.client.post(LOGIN_URL, payload)
+
+        self.assertNotIn('refresh', res.data)
+        self.assertNotIn('access', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_fail_unauthorized_user(self):
+        """Testing log-in fails and jwt tokens are not generated for unauthorized user"""
+        payload = {'email': 'unauthorized@testing.com', 'password': 'unauthorizedtestpass'}
+        res = self.client.post(LOGIN_URL, payload)
+
+        self.assertNotIn('refresh', res.data)
+        self.assertNotIn('access', res.data)   
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_refresh_token(self):
+        """Test refresh token is generated successfully"""
+        payload = {'email': 'test@testing.com', 'password': 'testpass'}
+        res = self.client.post(LOGIN_URL, payload)
+        refresh_token = {'refresh': res.data['refresh']}
+        refresh_res = self.client.post(REFRESH_URL, refresh_token)
+        self.assertNotIn('refresh', refresh_res.data)
+        self.assertIn('access', refresh_res.data)
+        self.assertEqual(refresh_res.status_code, status.HTTP_200_OK)
--- authentication/views.py
+++ authentication/views.py
@@ -1,20 +1,8 @@
-# from django.http import response
 from rest_framework import generics
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.permissions import IsAuthenticated
 from authentication.serializers import UserSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
     """Register a new user in the system"""
     serializer_class = UserSerializer
-
-
-# class HelloView(APIView):
-#     permission_classes = (IsAuthenticated,)
-
-#     def get(self, request):
-#         content = {'message': 'Hello World'}
-#         return Response(content)
